f66762196939c304f774928704850dd661af425b
dimaliwatkent <dimaliwatkent@gmail.com>
2024-01-03 12:24:32 +0800

    initial files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# cics-events
+# CICS Events
+
+## Installations
+
+1. Open Terminal
+2. `git clone https://github.com/dimaliwatkent/cics-events.git`
+3. `cd cics-events`
+4. 'npm install'
+
+## Run
+
+`npm run dev`
+
+## Live Site
+
+- [Firebase Live Site]()
---
+++ main_page/index.js
@@ -0,0 +1,29 @@
+const allEventsContainer = document.getElementById("clientEvents");
+
+// Load existing events from local storage
+let events = JSON.parse(localStorage.getItem("events")) || [];
+
+// Function to load all events
+function loadAllEvents() {
+  // Clear the current list of events
+  allEventsContainer.innerHTML = "";
+
+  // Loop through all events and add them to the page
+  events.forEach(function (event, index) {
+    const eventElement = document.createElement("div");
+    eventElement.innerHTML = `
+  <h2>${event.eventName}</h2>
+  <p>${event.eventDescription}</p>
+  `;
+    // Add a click event listener to the event div
+    eventElement.addEventListener("click", function () {
+      // Redirect to the event details page with the event index as a query parameter
+      window.location.href = "event_details/index.html?eventIndex=" + index;
+    });
+
+    allEventsContainer.appendChild(eventElement);
+  });
+}
+window.onload = () => {
+  loadAllEvents();
+};
